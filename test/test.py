7f084b57ddf06a53727fb21b0ec2b6c9ec3eee83
Sandwich-God <reubensandwichclemens@gmail.com>
2021-03-29 23:06:38 -0400

    update
    changed names to begin with OB for the white dots. Added a third one, trying to figure out a system that will pick one object and randomly choose which row it will be produced in.

--- test/test.py
+++ test/test.py
@@ -28,14 +28,19 @@ jump = False
 jumpCount = 10
 
 OBx = displayW - 70
-OBy = random.randrange(displayH - 150, displayH - 100)
+OBy = displayH - 80
 OBw = 40
 OBh = 40
 
-Colx = displayW - 70
-Coly = random.randrange(displayH - 400, displayH - 270)
-ColW = 40
-ColH = 40
+OB2x = displayW - 70
+OB2y = displayH - 150
+OB2w = 40
+OB2h = 40
+
+OB3x = displayW - 70
+OB3y = displayH - 220
+OB3w = 40
+OB3h = 40
 
 on = True
 
@@ -47,10 +52,13 @@ def player():
 	pygame.draw.rect(Display, red, (x, y, width, height))
 
 def Object():
-	pygame.draw.rect(Display, white, (Colx, Coly, ColW, ColH))
+	pygame.draw.rect(Display, white, (OBx, OBy, OBw, OBh))
 
 def Object2():
-	pygame.draw.rect(Display, white, (OBx, OBy, OBw, OBh))
+	pygame.draw.rect(Display, white, (OB2x, OB2y, OB2w, OB2h))
+
+def Object3():
+	pygame.draw.rect(Display, white, (OB3x, OB3y, OB3w, OB3h))
 
 def movement():
 	global x, y, speed, width, jump, jumpCount
@@ -69,31 +77,23 @@ def movement():
 		else:
 			jumpCount = 10
 			jump = False
-
+def test():
+	testv = random.randrange(1, 3)
+	if testv == 1:
+		Object()
+		ObjectCollision()
+	if testv == 2:
+		Object2()
+		Object2Collision()
+	if testv == 3:
+		Object3()
+		Object3Collision()
 
 def ObjectCollision():
-	global Colx, Coly, DisplayH, DisplayW, x, y
-	if Colx < 30:
-		Colx = displayW - 70
-		Coly = random.randrange(displayH - 400, displayH - 270)
-		
-	if x > Colx and x < Colx + 40 and y > Coly and y < Coly + 40:
-		hit()
-
-	elif x > Colx and x < Colx and y + Coly > displayH/2 and y + 55 < Coly + 40:
-		hit()
-		
-	elif x + 55> Colx and x < Colx + 40 and y > Coly and y < Coly + 40:
-		hit()
-
-	elif x + 55> Colx and x < Colx + 40 and y + 55 > Coly and y < Coly + 40:
-		hit()
-
-def Object2Collision():
 	global OBx, OBy, DisplayH, DisplayW, x, y
 	if OBx < 30:
 		OBx = displayW - 70
-		OBy = random.randrange(displayH - 150, displayH - 100)
+		OBy = displayH - 80
 		
 	if x > OBx and x < OBx + 40 and y > OBy and y < OBy + 40:
 		hit()
@@ -107,6 +107,42 @@ def Object2Collision():
 	elif x + 55> OBx and x < OBx + 40 and y + 55 > OBy and y < OBy + 40:
 		hit()
 
+def Object2Collision():
+	global OB2x, OB2y, DisplayH, DisplayW, x, y
+	if OB2x < 30:
+		OB2x = displayW - 70
+		OB2y = displayH - 150
+		
+	if x > OB2x and x < OB2x + 40 and y > OB2y and y < OB2y + 40:
+		hit()
+
+	elif x > OB2x and x < OB2x and y + OB2y > displayH/2 and y + 55 < OB2y + 40:
+		hit()
+		
+	elif x + 55> OB2x and x < OB2x + 40 and y > OB2y and y < OB2y + 40:
+		hit()
+
+	elif x + 55> OB2x and x < OB2x + 40 and y + 55 > OB2y and y < OB2y + 40:
+		hit()
+
+def Object3Collision():
+	global OB3x, OB3y, DisplayH, DisplayW, x, y
+	if OB3x < 30:
+		OB3x = displayW - 70
+		OB3y = displayH - 220
+		
+	if x > OB3x and x < OB3x + 40 and y > OB3y and y < OB3y + 40:
+		hit()
+
+	elif x > OB3x and x < OB3x and y + OB3y > displayH/2 and y + 55 < OB3y + 40:
+		hit()
+		
+	elif x + 55> OB3x and x < OB3x + 40 and y > OB3y and y < OB3y + 40:
+		hit()
+
+	elif x + 55> OB3x and x < OB3x + 40 and y + 55 > OB3y and y < OB3y + 40:
+		hit()
+
 def Boundarybox():
 	pygame.draw.rect(Display, blue, (0, 0, 30, displayH)) #left
 	pygame.draw.rect(Display, blue, (displayW, 0, -30, displayH)) #right
@@ -114,38 +150,37 @@ def Boundarybox():
 	pygame.draw.rect(Display, blue, (0, 0, displayW, 30)) #top
 
 def fonttype(point, font):
-	fontcolor = font.render(point, True, white)
-	return fontcolor, fontcolor.get_rect()
+	fontOBor = font.render(point, True, white)
+	return fontOBor, fontOBor.get_rect()
 
 def hit():
 	global on, displayW, displayH
 	Display.fill(black)
 	largeText = pygame.font.SysFont('arial',115)
-	fontcolor, TextRect = fonttype("Lost", largeText)
+	fontOBor, TextRect = fonttype("Lost", largeText)
 	TextRect.center = ((displayW/2),(displayH/2))
-	Display.blit(fontcolor, TextRect)
+	Display.blit(fontOBor, TextRect)
 	pygame.display.update()
 	clock.tick(15)
 	time.sleep(2)
 	on = False
 
 def game():
-	global on, points, Colx, Coly, OBx
+	global on, points, OBx, OB2x, OB3x
 	on = True
 	while on:
-		Colx -= 10
 		OBx -= 10
+		OB2x -= 10
+		OB3x -= 10
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				on = False
-		ObjectCollision()
-		Object2Collision()
+		
 		movement()
 		Display.fill(black)
 		Boundarybox()
+		test()
 		player()
-		Object()
-		Object2()
 		pygame.display.update()
 		clock.tick(75)
 		QorE = pygame.key.get_pressed()
